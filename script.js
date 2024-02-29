39ab3adadf78668ae80082c1e1a93cc657d2ea36
sveta <grischuk6@gmail.com>
2024-02-29 16:34:12 +0700

    Реализован показ диалогового окна, а также подсветка верных/неверных ответов.

--- script.js
+++ script.js
@@ -8,6 +8,29 @@ function checkAnswer(button) {
     // Подсвечиваем выбранную кнопку
     button.classList.add('selected');
 
-    // Здесь можно добавить логику для проверки правильного ответа
-    // и выполнения соответствующих действий.
 }
+
+function checkAnswers() {
+    var correctAnswer = " Из тегов и контента "; // Здесь нужно указать правильный ответ
+
+    var selectedButton = document.querySelector('.but.selected');
+    if (selectedButton) {
+        selectedButton.classList.remove('selected');
+        if (selectedButton.textContent === correctAnswer) {
+            selectedButton.classList.add('correct');
+            window.alert("Ты молодец, так держать !");
+
+        } else {
+            selectedButton.classList.add('incorrect');
+            window.alert("К сожалению ты ошибся :(");
+        }
+    }
+
+    var allButtons = document.querySelectorAll('.but');
+    allButtons.forEach(function (but) {
+        if (but.textContent === correctAnswer) {
+            but.classList.add('correct');
+        }
+    });
+}
+
